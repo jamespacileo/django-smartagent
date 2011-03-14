3be3587b9f1c0adb4cc774f8855e0bd522241112
James Pacileo <jamespacileo@gmail.com>
2011-03-14 14:13:18 -0700

    Made more characteristics available through the request object

--- django_smartagent/middleware.py
+++ django_smartagent/middleware.py
@@ -1,4 +1,11 @@
-from useragent_detector.utils import detect_user_agent
+from django_smartagent.utils import detect_user_agent
+
+NEEDED_VALUES = set(['browser', 'majorver', 'minorver', 'cookies',
+'activexcontrols', 'cdf', 'parent_index', 'supportscss', 'aolversion',
+'frames', 'cssversion', 'isbanned', 'tables', 'iframes', 'vbscript',
+'ismobiledevice', 'platform', 'version', 'aol', 'javaapplets', 'parent',
+'backgroundsounds', 'win64', 'javascript', 'beta', 'alpha',
+'issyndicationreader', 'win32', 'depth', 'crawler', 'win16'])
 
 class UserAgentDetectorMiddleware(object):
 
@@ -6,14 +13,12 @@ class UserAgentDetectorMiddleware(object):
         """
         Add browser features to request object
         """
-        user_agent = request.META['HTTP_USER_AGENT']
-        print user_agent
-        user_agent = detect_user_agent(user_agent)
+        _user_agent = request.META['HTTP_USER_AGENT']
+        _user_agent = detect_user_agent(_user_agent)
+        user_agent = _user_agent.copy()
 
-        request.is_mobile = False
-        if user_agent.has_key('ismobiledevice'):
-            if user_agent['ismobiledevice'] in ['true', True]:
-                request.is_mobile = True
-        
-        if request.GET.has_key('mobile'):
-            request.is_mobile = True
+        keys = user_agent.keys()
+        for key in keys:
+            if key not in NEEDED_VALUES:
+                user_agent.pop(key)
+        request.browser_data = user_agent
